bfb7a0d37f11b612177bab6133b2d4424cf35144
Paulo Xavier <contato.pauloxavier@gmail.com>
2018-03-25 20:58:30 -0300

    Primeiros experimentos com react

--- tutorial/src/index.js
+++ tutorial/src/index.js
@@ -1,3 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+
+function GenericList(props){
+    const numbers = props.numbers;
+
+    const listItems = numbers.map((number)=>
+        <li key={number.toString()}>{number}</li>
+    );
+
+    return (
+        <ul>{listItems}</ul>
+    );
+}
+
+const numbers = [1,2,3,4,5];
+
+  ReactDOM.render(
+    <GenericList numbers={numbers}/>,
+    document.getElementById('root')
+  );
+  
